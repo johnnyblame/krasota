7b199ca50cb21398d416e3e87d10481c6a553307
johnnyblame <iwishmynamewasdaniel@gmail.com>
2018-10-15 20:18:05 +0300

    tab 1 finised + nasrano neponyatno sho

--- hzsho.py
+++ hzsho.py
@@ -1,7 +1,11 @@
+import random
 import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, QLabel, QLineEdit,  qApp, QApplication, QPushButton, QWidget, QTextEdit, QGridLayout,  QTabWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QAction, QLabel, QMenu, QSizePolicy, QLineEdit,  qApp, QApplication, QCheckBox, QPushButton, QWidget, QTextEdit, QGridLayout,  QTabWidget, QVBoxLayout
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, Qt, QCoreApplication
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 class App(QMainWindow):
 
@@ -14,7 +18,9 @@ class App(QMainWindow):
         self.table_widget = MyTableWidget(self)
         self.setCentralWidget(self.table_widget)
 
+
         self.show()
+
 
 class MyTableWidget(QWidget):
 
@@ -45,7 +51,65 @@ class MyTableWidget(QWidget):
         self.tab1.grid.addWidget(self.tab1.data1Edit, 1, 1)
         self.tab1.grid.addWidget(self.tab1.data2, 0, 2)
         self.tab1.grid.addWidget(self.tab1.data2Edit, 1, 2)
+        self.tab1.cb = QCheckBox('sraka', self)
+        self.tab1.grid.addWidget(self.tab1.cb, 0, 3)
+        self.tab1.cb.toggle()
+        self.tab1.cb1 = QCheckBox('hui', self)
+        self.tab1.cb1.toggle()
+        self.tab1.grid.addWidget(self.tab1.cb1, 1, 3)
         self.tab1.setLayout(self.tab1.grid)
+
+
+        #tab2
+
+
+        #self.tab2.gr = PlotCanvas(self, width=5, height=4)
+
+
+
+        #self.tab2.gr.plot()
+        #self.tab2.gr.draw()
+
+        #tab3
+        self.tab3.qbtn = QPushButton('Quit', self)
+        self.tab3.qbtn.clicked.connect(QCoreApplication.instance().quit)
+        self.tab3.qbtn.resize(self.tab3.qbtn.sizeHint())
+        self.tab3.grid = QGridLayout()
+        self.tab3.grid.setSpacing(10)
+        self.tab3.grid.addWidget(self.tab3.qbtn, 3, 0)
+        self.tab3.setLayout(self.tab3.grid)
+        self.tab3.ibtn = QPushButton('Import', self)
+        self.tab3.ibtn.clicked.connect(QCoreApplication.instance().quit)
+        self.tab3.grid.addWidget(self.tab3.ibtn, 2, 0)
+        self.tab3.ebtn = QPushButton('Export', self)
+        self.tab3.grid.addWidget(self.tab3.ebtn, 1, 0)
+        self.tab3.ebtn.resize(self.tab3.ebtn.sizeHint())
+
+
+
+
+
+
+#    def showDialog(self):
+#        self.tab3.fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
+#
+#        self.tab3.f = open(self.tab3.fname, 'r')
+#
+#        with self.tab3.f:
+#            data = self.tab3.f.read()
+#            self.textEdit.setText(data)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -54,11 +118,33 @@ class MyTableWidget(QWidget):
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
 
-        @pyqtSlot()
-        def on_click(self):
-            print("\n")
-            for currentQTableWidgetItem in self.tableWidget.selectedItems():
-                print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
+
+#class PlotCanvas(FigureCanvas):
+
+#    def __init__(self, parent=None, width=5, height=4, dpi=100):
+#        fig = Figure(figsize=(width, height), dpi=dpi)
+#        self.axes = fig.add_subplot(111)
+
+#        FigureCanvas.__init__(self, fig)
+#        self.setParent(parent)
+
+#        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+#        FigureCanvas.updateGeometry(self)
+
+
+
+#    def plot(self):
+#        daata = [random.random() for i in range(25)]
+#        ax = self.figure.add_subplot(111)
+#        ax.plot(daata, 'r-')
+#        ax.set_title('sho proishodit')
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
